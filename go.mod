70fbaaf75b3faeab176a903679fda365fabfd0d4
ovijeet <osircar@endor.ai>
2024-09-09 11:52:21 +0530

    feat: init repo

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/ovi-github-organization/fx-demo
+
+go 1.22.5
+
+require (
+	go.uber.org/fx v1.22.2
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+)
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
+)
+
+func main() {
+	app := fx.New(
+		fx.Provide(
+			NewHTTPServer,
+			fx.Annotate(
+				NewServeMux,
+				fx.ParamTags(`name:"echo"`, `name:"hello"`),
+			),
+			fx.Annotate(
+				NewEchoHandler,
+				fx.As(new(Route)),
+				fx.ResultTags(`name:"echo"`),
+			),
+			fx.Annotate(
+				NewHelloHandler,
+				fx.As(new(Route)),
+				fx.ResultTags(`name:"hello"`),
+			),
+			zap.NewExample,
+		),
+		fx.Invoke(func(*http.Server) {}),
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+	)
+	app.Run()
+}
+
+// NewHTTPServer builds an HTTP server that will begin serving requests
+// when the Fx application starts.
+// Add a lifecycle hook to the application with the fx.Lifecycle object. This tells Fx how to start and stop the HTTP server.
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+
+	return srv
+}
+
+// Route is an http.Handler that knows the mux pattern
+// under which it will be registered.
+type Route interface {
+	http.Handler
+
+	// Pattern reports the path at which this is registered.
+	Pattern() string
+}
+
+// NewServeMux builds a ServeMux that will route requests
+// to the given routes.
+func NewServeMux(route1, route2 Route) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle(route1.Pattern(), route1)
+	mux.Handle(route2.Pattern(), route2)
+	return mux
+}
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct {
+	log *zap.Logger
+}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+	return &EchoHandler{log: log}
+}
+
+func (*EchoHandler) Pattern() string {
+	return "/echo"
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		h.log.Warn("Failed to handle request", zap.Error(err))
+	}
+}
+
+// HelloHandler is an HTTP handler that
+// prints a greeting to the user.
+type HelloHandler struct {
+	log *zap.Logger
+}
+
+// NewHelloHandler builds a new HelloHandler.
+func NewHelloHandler(log *zap.Logger) *HelloHandler {
+	return &HelloHandler{log: log}
+}
+
+func (*HelloHandler) Pattern() string {
+	return "/hello"
+}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Error("Failed to read request", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "Hello, %s\n", body); err != nil {
+		h.log.Error("Failed to write response", zap.Error(err))
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
